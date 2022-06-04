ddb5cc42a79d6e7d216134fde21df4dbc28cb89c
Melody Garcia <garcia.a.melody@gmail.com>
2022-06-04 18:47:10 -0500

    added character info

--- server.js
+++ server.js
@@ -8,492 +8,492 @@ app.use(cors())
 const characters = {
     'chrom':{
         'startingClass': 'Lord',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'Falchion'
     },
     'lissa':{
         'startingClass': 'Cleric',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'Staff'  
     },
     'panne':{
         'startingClass': 'Taguel',
-        'nationality': 'Taguel',
+        'birthPlace': 'Taguel',
         'weapon': 'Beast'  
     },
     'frederick':{
         'startingClass': 'Great Knight',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'Silver Lance'  
     },
     'tiki':{
         'startingClass': 'Manakete',
-        'nationality': 'Divine Dragon',
+        'birthPlace': 'Divine Dragon',
         'weapon': 'Dragonstone'  
     },
     'virion':{
         'startingClass': 'Archer',
-        'nationality': 'Valm',
+        'birthPlace': 'Valm',
         'weapon': 'bow'  
     },
     'sully':{
         'startingClass': 'Cavalier',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'lance'  
     },
     'vaike':{
         'startingClass': 'Fighter',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'Axe'  
     },
     'gaius':{
         'startingClass': 'Thief',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'Sword'  
     },
     'miriel':{
         'startingClass': 'Mage',
-        'nationality': 'Ylisse',
+        'birthPlace': 'Ylisse',
         'weapon': 'Tome'  
     },
     'kellam':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Knight',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Lance'  
     },
     'cordelia':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Pegasus Knight',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Lance'  
     },
     'flavia':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Hero',
+        'birthPlace': 'Ferox',
+        'weapon': 'Sword'  
     },
     'libra':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'War Monk',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Axe'  
     },
     'olivia':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Dancer',
+        'birthPlace': 'Ferox',
+        'weapon': 'Sword'  
     },
     'tharja':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Dark Mage',
+        'birthPlace': 'Plegia',
+        'weapon': 'Tome'  
     },
     "lon'qu":{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Myrmidon',
+        'birthPlace': 'Ferox',
+        'weapon': 'Killing Edge'  
     },
     'nowi':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Manakete',
+        'birthPlace': 'Unknown',
+        'weapon': 'Dragonstone'  
     },
     'gregor':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Mercenary',
+        'birthPlace': 'Ferox',
+        'weapon': 'Sword'  
     },
     'sumia':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Pegasus Knight',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Lance'  
     },
     'henry':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Dark Mage',
+        'birthPlace': 'Plegia',
+        'weapon': 'Tome'  
     },
     'cherche':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Wyvern Rider',
+        'birthPlace': 'Valm',
+        'weapon': 'Axe'  
     },
     'priam':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Hero',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Sword'  
     },
     'ricken':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Mage',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Tome'  
     },
     'maribelle':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Troubadour',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Staff'  
     },
     'basilio':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Warrior',
+        'birthPlace': 'Ferox',
+        'weapon': 'Axe'  
     },
     "yen'fay":{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'startingClass': 'Swordmaster',
+        'birthPlace': "Chon'sin",
         'weapon': 'Chicago, Illinois'  
     },
     "say'ri":{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Swordmaster',
+        'birthPlace': "Chon'sin",
+        'weapon': 'Sword'  
     },
     'stahl':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Cavalier',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Sword'  
     },
     'gangrel':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Trickster',
+        'birthPlace': 'Plegia',
+        'weapon': 'Sword'  
     },
     'aversa':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Dark Flier',
+        'birthPlace': 'Plegia',
+        'weapon': 'Tome'  
     },
     'walhart':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Conqueror',
+        'birthPlace': 'Valm',
+        'weapon': 'Sol'  
     },
     'emmeryn':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Sage',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Staff'  
     },
     'anna':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Trickster',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Sword'  
     },
     'donnel':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Villager',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Lance'  
     },
     'lucina':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Lord',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Parallel Falchion'  
     },
     'gerome':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Wyvern Rider',
+        'birthPlace': 'Unknown',
+        'weapon': 'Axe'  
     },
     'owain':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Myrmidon',
+        'birthPlace': 'Ylisse',
+        'weapon': 'Sword'  
     },
     'kjelle':{
-        'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
-        'weapon': 'Chicago, Illinois'  
+        'startingClass': 'Knight',
+        'birthPlace': 'Unknown',
+        'weapon': 'Lance'  
     },
     'inigo':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'severa':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'noire':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'yarne':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'laurent':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'brady':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'nah':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'cynthia':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'morgan':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'validar':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'phila':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'hierarch':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'naga':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'gyral':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'dalen':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'old hubba':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'grima':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'cervantes':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'excellus':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'garrick':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'raimi':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'roddick':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'victor':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'vincent':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'orton':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'vasto':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'chalard':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'campari':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'mustafa':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'dalton':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'gecko':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'jamil':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'xalbador':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'cassius':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'ruger':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'nelson':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'holland':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'morristan':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'nombry':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'ezra':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'ignatius':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'farber':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'pheros':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'algol':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'zanth':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'ardri':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'mus':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'bovis':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'tigris':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'lepus':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'draco':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'anguilla':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'equus':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'ovis':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'simia':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'gallus':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'canis':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'porcus':{
         'startingClass': 29,
-        'nationality': 'Chancelor Bennet',
+        'birthPlace': 'Chancelor Bennet',
         'weapon': 'Chicago, Illinois'  
     },
     'unknown':{
         'startingClass': 0,
-        'nationality': 'unknown',
+        'birthPlace': 'unknown',
         'weapon': 'unknown'
     }
 }
